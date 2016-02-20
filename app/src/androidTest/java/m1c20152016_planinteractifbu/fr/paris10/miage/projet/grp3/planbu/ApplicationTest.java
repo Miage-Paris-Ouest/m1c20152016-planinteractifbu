b56c2d30da3047d46ed4b13504a6db6ddc0017f3
Mohamed-MIAGE <mohamed.hajismail@gmail.com>
2016-02-20 16:30:51 +0100

    Remise a zero de la branche Momo

---
+++ app/src/androidTest/java/m1c20152016_planinteractifbu/fr/paris10/miage/projet/grp3/planbu/ApplicationTest.java
@@ -0,0 +1,13 @@
+package m1c20152016_planinteractifbu.fr.paris10.miage.projet.grp3.planbu;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
