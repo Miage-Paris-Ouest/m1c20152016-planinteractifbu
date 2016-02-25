d4aefedc68d5242990e8b8e04e1c8c29b3fbdd14
ClementBu <cle.burg@hotmail.fr>
2016-02-25 17:08:00 +0100

    Modification classe bibliothere.salle.Salle

--- app/src/main/java/grp3/projet/miage/paris10/fr/m1c20152016_planinteractifbu/bibliotheque/salle/Salle.java
+++ app/src/main/java/grp3/projet/miage/paris10/fr/m1c20152016_planinteractifbu/bibliotheque/salle/Salle.java
@@ -7,23 +7,13 @@ import grp3.projet.miage.paris10.fr.m1c20152016_planinteractifbu.leplan.represen
 
 public class Salle {
     private String nom;
-    private Set<Service> services;
     private Rectangle representation;
     private boolean afficherNom;
 
-    public Salle(String nom, int left, int top, int right, int bottom, int color, float borderWidth, int borderColor, boolean afficherNom) {
+    public Salle(String nom, int x, int y, int l, int h, int color, float largeurContour, int couleurContour, boolean afficherNom) {
         this.nom = nom;
-        services = new HashSet<>();
-        representation = new Rectangle(left, top, right, bottom, color, borderWidth, borderColor);
+        representation = new Rectangle(x, y, l, h, color, largeurContour, couleurContour);
         this.afficherNom = afficherNom;
-    }
-
-    public void addService(Service service) {
-        services.add(service);
-    }
-
-    public Set<Service> getServices() {
-        return services;
     }
 
     public String getNom() {
