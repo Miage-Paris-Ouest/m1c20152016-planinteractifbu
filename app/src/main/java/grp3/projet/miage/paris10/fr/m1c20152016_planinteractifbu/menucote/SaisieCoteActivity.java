44626ab58c15e9a5f55645b4255c2902d2168823
ClementBu <cle.burg@hotmail.fr>
2016-02-28 00:53:14 +0100

    Création de la classe activité de recherche de cote + ajout de l'activité dans l'android manifest.

---
+++ app/src/main/java/grp3/projet/miage/paris10/fr/m1c20152016_planinteractifbu/menucote/SaisieCoteActivity.java
@@ -0,0 +1,34 @@
+package grp3.projet.miage.paris10.fr.m1c20152016_planinteractifbu.menucote;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import grp3.projet.miage.paris10.fr.m1c20152016_planinteractifbu.R;
+import grp3.projet.miage.paris10.fr.m1c20152016_planinteractifbu.plan.PlanActivity;
+
+public class SaisieCoteActivity extends Activity {
+    EditText editTextSaisie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_cote);
+
+        editTextSaisie = (EditText) findViewById(R.id.editTextCote);
+        Button validateCoteButton = (Button) findViewById(R.id.buttonValidCote);
+
+        validateCoteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(!editTextSaisie.getText().toString().isEmpty()) {
+                    Intent intentPlanInteractif = new Intent(SaisieCoteActivity.this, PlanActivity.class);
+                    intentPlanInteractif.putExtra("coteSaisie", editTextSaisie.getText().toString());
+                    startActivity(intentPlanInteractif);
+                }
+            }
+        });
+    }
+}
