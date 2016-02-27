9fc1a422104436a2c478ed28ddb7037d371eab19
ClementBu <cle.burg@hotmail.fr>
2016-02-27 17:04:53 +0100

    Ajout des méthodes et propriérés permettant la représentation des étagères.

--- app/src/main/java/grp3/projet/miage/paris10/fr/m1c20152016_planinteractifbu/bibliotheque/cote/Etagere.java
+++ app/src/main/java/grp3/projet/miage/paris10/fr/m1c20152016_planinteractifbu/bibliotheque/cote/Etagere.java
@@ -1,17 +1,39 @@
 package grp3.projet.miage.paris10.fr.m1c20152016_planinteractifbu.bibliotheque.cote;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.graphics.Color;
+
+import grp3.projet.miage.paris10.fr.m1c20152016_planinteractifbu.bibliotheque.representations.Rectangle;
 
 public class Etagere {
     private int num;
-
+    private Rectangle representation;
+    public static int COULEUR = Color.WHITE;
+    public static float LARGEUR_CONTOUR = 2;
+    public static int COULEUR_CONTOUR = Color.BLACK;
 
     public Etagere(int num) {
         this.num = num;
     }
 
+    public Etagere(int num, int x, int y, int l, int h, int color, float largeurContour, int couleurContour) {
+        this.num = num;
+        representation = new Rectangle(x, y, l, h, color, largeurContour, couleurContour);
+    }
+
     public int getNum() {
         return num;
+    }
+
+    public Rectangle getRepresentation() {
+        return representation;
+    }
+
+    public void setRepresentation(Rectangle representation) {
+        this.representation = representation;
+    }
+
+    public void faireRessortir() {
+        representation.setColor(Color.rgb(255, 127, 39));
+        //representation.setCouleurContour(Color.rgb(255, 127, 39));
     }
 }
